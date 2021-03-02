26ec35e87baaeac8b64b0b55c48e1701e5a5c06f
lucemarchal <79578106+lucemarchal@users.noreply.github.com>
2021-03-02 18:40:53 +0100

    Modifs Luce

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,48 +3,48 @@ const subRegion = document.getElementById('subRegion');
 const thisPath = document.getElementById('layer1');
 
 
-document.querySelector(".self-esteem").onmouseover = function () { getInfo('Aspiring', this.className.baseVal, this); };
-document.querySelector(".approbativeness").onmouseover = function () { getInfo('Aspiring', this.className.baseVal) };
-document.querySelector(".cautiousness").onmouseover = function () { getInfo('Aspiring', this.className.baseVal) };
-document.querySelector(".firmness").onmouseover = function () { getInfo('Aspiring', this.className.baseVal) };
-document.querySelector(".conscientiousness").onmouseover = function () { getInfo('Aspiring', this.className.baseVal) };
-document.querySelector(".sublimity").onmouseover = function () { getInfo('Aspiring', this.className.baseVal) };
-document.querySelector(".veneration").onmouseover = function () { getInfo('Moral', this.className.baseVal) };
-document.querySelector(".benevolence").onmouseover = function () { getInfo('Moral', this.className.baseVal) };
-document.querySelector(".hope").onmouseover = function () { getInfo('Moral', this.className.baseVal) };
-document.querySelector(".spirituality").onmouseover = function () { getInfo('Moral', this.className.baseVal) };
-document.querySelector(".imitation").onmouseover = function () { getInfo('Moral', this.className.baseVal) };
-document.querySelector(".human-nature").onmouseover = function () { getInfo('Reflectives', this.className.baseVal) };
-document.querySelector(".agreeableness").onmouseover = function () { getInfo('Reflectives', this.className.baseVal) };
-document.querySelector(".causality").onmouseover = function () { getInfo('Reflectives', this.className.baseVal) };
-document.querySelector(".comparativeness").onmouseover = function () { getInfo('Reflectives', this.className.baseVal) };
-document.querySelector(".bibativeness").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".tune").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".time").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".locality").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".eventuality").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".calculation").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".order").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".color").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".weight").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".size").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".individuality").onmouseover = function () { getInfo('Percertives', this.className.baseVal) };
-document.querySelector(".mindfullness").onmouseover = function () { getInfo('Self Perfecting', this.className.baseVal) };
-document.querySelector(".mirthfulness").onmouseover = function () { getInfo('Self Perfecting', this.className.baseVal) };
-document.querySelector(".ideality").onmouseover = function () { getInfo('Self Perfecting', this.className.baseVal) };
-document.querySelector(".constructiveness").onmouseover = function () { getInfo('Self Perfecting', this.className.baseVal) };
-document.querySelector(".continuity").onmouseover = function () { getInfo('Domest', this.className.baseVal) };
-document.querySelector(".freindship").onmouseover = function () { getInfo('Domest', this.className.baseVal) };
-document.querySelector(".conjugality").onmouseover = function () { getInfo('Domest', this.className.baseVal) };
-document.querySelector(".amativeness").onmouseover = function () { getInfo('Domest', this.className.baseVal) };
-document.querySelector(".parental-love").onmouseover = function () { getInfo('Domest', this.className.baseVal) };
-document.querySelector(".inhabitiveness").onmouseover = function () { getInfo('Domest', this.className.baseVal) };
-document.querySelector(".vitativeness").onmouseover = function () { getInfo('Animal', this.className.baseVal) };
-document.querySelector(".combativeness").onmouseover = function () { getInfo('Animal', this.className.baseVal) };
-document.querySelector(".secretiveness").onmouseover = function () { getInfo('Animal', this.className.baseVal) };
-document.querySelector(".aquisitiveness").onmouseover = function () { getInfo('Animal', this.className.baseVal) };
-document.querySelector(".destructiveness").onmouseover = function () { getInfo('Animal', this.className.baseVal) };
-document.querySelector(".alimentiveness").onmouseover = function () { getInfo('Animal', this.className.baseVal) };
+document.querySelector(".GabrielPicolo").onmouseover = function () { getInfo('Rêverie','Gabriel Picolo'); };
+document.querySelector(".HarryPotter").onmouseover = function () { getInfo('Rêverie', 'Harry Potter, J.K Rowling') };
+document.querySelector(".SoniaDelaunay").onmouseover = function () { getInfo('Rêverie', 'Sonia Delaunay') };
+document.querySelector(".firmness").onmouseover = function () { getInfo('Rêverie', 'mon titre') };
+document.querySelector(".conscientiousness").onmouseover = function () { getInfo('Rêverie', 'mon titre') };
+document.querySelector(".sublimity").onmouseover = function () { getInfo('Rêverie', 'mon titre') };
+document.querySelector(".U-turn").onmouseover = function () { getInfo('Mélancolie', 'U-turn (Lili),  AaRON') };
+document.querySelector(".IsaureChassériau").onmouseover = function () { getInfo('Mélancolie', 'Isaure Chassériau, Eugène Amaury-Duval') };
+document.querySelector(".EternalSunshine").onmouseover = function () { getInfo('Mélancolie', 'Eternal Sunshine of the Spotless Mind') };
+document.querySelector(".spirituality").onmouseover = function () { getInfo('Mélancolie', 'mon titre') };
+document.querySelector(".imitation").onmouseover = function () { getInfo('Mélancolie', 'mon titre') };
+document.querySelector(".MartinParr").onmouseover = function () { getInfo('Paresse', 'Martin Parr') };
+document.querySelector(".Lesrigoles").onmouseover = function () { getInfo('Paresse', 'Les rigoles, Brecht Evens') };
+document.querySelector(".Friends").onmouseover = function () { getInfo('Paresse', 'Friends (la série), Marta Kauffman et David Crane') };
+document.querySelector(".comparativeness").onmouseover = function () { getInfo('Paresse', 'mon titre') };
+document.querySelector(".ThérapieTaxi").onmouseover = function () { getInfo('Créativité', 'Thérapie Taxi') };
+document.querySelector(".CassandraCalin").onmouseover = function () { getInfo('Créativité', 'Cassandra Calin') };
+document.querySelector(".Elmer").onmouseover = function () { getInfo('Créativité', 'Elmer, David McKee') };
+document.querySelector(".locality").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".eventuality").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".calculation").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".order").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".color").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".weight").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".size").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".individuality").onmouseover = function () { getInfo('Créativité', 'mon titre') };
+document.querySelector(".Bookworm").onmouseover = function () { getInfo('Optimisme', 'L’étagère Bookworm, Ron Arad') };
+document.querySelector(".mirthfulness").onmouseover = function () { getInfo('Optimisme', 'mon titre') };
+document.querySelector(".ideality").onmouseover = function () { getInfo('Optimisme', 'mon titre') };
+document.querySelector(".constructiveness").onmouseover = function () { getInfo('Optimisme', 'mon titre') };
+document.querySelector(".Davidlachapelle").onmouseover = function () { getInfo('Joie', 'David la chapelle, Great Joy for all') };
+document.querySelector(".freindship").onmouseover = function () { getInfo('Joie', 'Le fabuleux destin d’Amélie Poulain, Jean-Pierre Jeunet') };
+document.querySelector(".MathurinMéheut").onmouseover = function () { getInfo('Joie', 'Mathurin Méheut') };
+document.querySelector(".désavoeux").onmouseover = function () { getInfo('Joie', 'Les cartes de désavoeux, Garage Deloffre') };
+document.querySelector(".parental-love").onmouseover = function () { getInfo('Joie', 'mon titre') };
+document.querySelector(".inhabitiveness").onmouseover = function () { getInfo('Joie', 'mon titre') };
+document.querySelector(".Transfert").onmouseover = function () { getInfo('Curiosité', 'Podcast Transfert de Slate.fr') };
+document.querySelector(".combativeness").onmouseover = function () { getInfo('Curiosité', 'Le Bauhaus, 1919 - 1936') };
+document.querySelector(".Guggenheim").onmouseover = function () { getInfo('Curiosité', 'Les musées Guggenheim (ceux de Bilbao et New York)') };
+document.querySelector(".Louvre").onmouseover = function () { getInfo('Curiositée', "Musée du Louvre d'Abu Dhabi") };
+document.querySelector(".destructiveness").onmouseover = function () { getInfo('Curiosité', 'mon titre') };
+document.querySelector(".alimentiveness").onmouseover = function () { getInfo('Curiosité', 'mon titre') };
 
 
 
@@ -71,6 +71,6 @@ function getInfo(regionName, classname, pathBrain) {
 
     region.innerHTML = regionName;
     subRegion.innerHTML = classname;
-    this.onmouseout = function () { region.innerHTML = "Hover over a region"; subRegion.innerHTML = "" }
+    this.onmouseout = function () { region.innerHTML = "Mon univers en références"; subRegion.innerHTML = "" }
 
 }
